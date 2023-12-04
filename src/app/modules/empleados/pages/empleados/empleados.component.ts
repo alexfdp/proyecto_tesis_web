38d53ec76cfe84f72be634e107325ffdd557977d
Alex Fajardo <alexf.124@hotmail.com>
2023-12-04 17:29:29 -0500

    commit 14

--- src/app/modules/empleados/pages/empleados/empleados.component.ts
+++ src/app/modules/empleados/pages/empleados/empleados.component.ts
@@ -2,11 +2,13 @@ import { Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableDataSource } from '@angular/material/table';
 import { SharedModule } from 'src/app/shared/shared/shared.module';
 import { EmpleadosService } from '../../services/empleados.service';
 import { Empleado } from 'src/app/models/Empleado';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe, NgIf } from '@angular/common';
+import { FormBuilder, FormsModule, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
@@ -72,8 +74,77 @@ export class EmpleadosComponent {
   templateUrl: 'add-empleado-dialog.html',
   styleUrls: ['./empleados.component.scss'],
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, NgIf, FormsModule, ReactiveFormsModule],
 })
 export class AddEmpleadoDialog {
+  public myForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.createMyForm();
+  }
+
+  public get f(): any {
+    return this, this.myForm.controls;
+  }
+
+  private createMyForm(): FormGroup {
+    return this.fb.group({
+      nombre: ['', [Validators.required, Validators.maxLength(45)]],
+      apellido: ['', [Validators.required, Validators.maxLength(45)]],
+      apellido_2: ['', Validators.required, Validators.maxLength(45)],
+      cedula: ['', [Validators.required, Validators.maxLength(10)]],
+      direccion: ['', [Validators.required, Validators.maxLength(200)]],
+      telefono: [null, [Validators.required, Validators.minLength(10)]],
+      correo: ['', [Validators.required, Validators.maxLength(200), Validators.email]],
+      sueldo: ['', [Validators.required]],
+      usuario: ['', [Validators.required, Validators.maxLength(20)]]
+    });
+  }
+
+  submitFormulario() {
+    if (this.myForm.invalid) {
+      Object.values(this.myForm.controls).forEach(control => {
+        console.log(control.errors);
+        control.markAllAsTouched();
+      });
+      return;
+    }
+  }
+  cedula = ""
+  public validador!: Boolean;
+  validadorDeCedula(cedula: String) {
+    let cedulaCorrecta = false;
+    if (cedula.length == 10) {
+      let tercerDigito = parseInt(cedula.substring(2, 3));
+      if (tercerDigito < 6) {
+        // El ultimo digito se lo considera dígito verificador
+        let coefValCedula = [2, 1, 2, 1, 2, 1, 2, 1, 2];
+        let verificador = parseInt(cedula.substring(9, 10));
+        let suma: number = 0;
+        let digito: number = 0;
+        for (let i = 0; i < (cedula.length - 1); i++) {
+          digito = parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
+          suma += ((parseInt((digito % 10) + '') + (parseInt((digito / 10) + ''))));
+          //      console.log(suma+" suma"+coefValCedula[i]); 
+        }
+        suma = Math.round(suma);
+        //  console.log(verificador);
+        //  console.log(suma);
+        //  console.log(digito);
+        if ((Math.round(suma % 10) == 0) && (Math.round(suma % 10) == verificador)) {
+          cedulaCorrecta = true;
+        } else if ((10 - (Math.round(suma % 10))) == verificador) {
+          cedulaCorrecta = true;
+        } else {
+          cedulaCorrecta = false;
+        }
+      } else {
+        cedulaCorrecta = false;
+      }
+    } else {
+      cedulaCorrecta = false;
+    }
+    this.validador = cedulaCorrecta;
+
+  }
 }
